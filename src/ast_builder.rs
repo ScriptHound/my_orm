ec6852275dfc0c1a8c7e33c6fb810478224b296b
Kivooeo <Kivoeoo123@gmail.com>
2024-11-13 00:46:21 +0500

    fixed ast_builder

--- src/ast_builder.rs
+++ src/ast_builder.rs
@@ -5,43 +5,40 @@ enum StatementType {
     Select,
     Update,
     Delete,
-    Insert
-}
- 
-struct QueryBlock<'a> {
-    pub query_part: String,
-    pub secondary_part: &'a mut Option<Box<QueryBlock<'a>>>,
-    pub statement_type: StatementType
+    Insert,
 }
 
+struct QueryBlock {
+    pub query_part: String,
+    pub secondary_part: Option<Box<QueryBlock>>,
+    pub statement_type: StatementType,
+}
 
 struct Model {
     pub name: String,
-    pub fields: Option<Vec<String>>
+    pub fields: Option<Vec<String>>,
 }
-
 
 trait SecondaryPart {
-    fn values<'a>(&'a mut self, values: &Vec<String>) -> &'a QueryBlock;
-    fn where_clause<'a>(&'a mut self, where_clause: &String) -> &'a QueryBlock;
-    fn set<'a>(&'a mut self, arguments: &HashMap<String, String>) -> &'a QueryBlock;
+    fn values(self, values: &Vec<String>) -> Self;
+    fn where_clause(self, where_clause: &String) -> Self;
+    fn set(self, arguments: &HashMap<String, String>) -> Self;
 }
 
-
-impl<'a> SecondaryPart for QueryBlock<'a> {
-    fn values<'b>(&mut self, values: &'b Vec<String>) -> &'a QueryBlock {
+impl SecondaryPart for QueryBlock {
+    fn values(mut self, values: &Vec<String>) -> Self {
         let values_str = format!("VALUES ({})", values.join(", "));
         let query_block = QueryBlock {
             query_part: values_str,
-            secondary_part: &mut None,
-            statement_type: self.statement_type.clone()
+            secondary_part: None,
+            statement_type: self.statement_type.clone(),
         };
-        let latest_node = traverse_to_the_latest_node(self);
-        latest_node.secondary_part = &mut Some(Box::new(query_block));
+        let latest_node = traverse_to_the_latest_node(&mut self);
+        latest_node.secondary_part = Some(Box::new(query_block));
         self
     }
 
-    fn set(&mut self, arguments: &HashMap<String, String>) -> &QueryBlock {
+    fn set(mut self, arguments: &HashMap<String, String>) -> Self {
         let set_clause = arguments
             .iter()
             .map(|(key, value)| format!("{} = {}", key, value))
@@ -50,83 +47,79 @@ impl<'a> SecondaryPart for QueryBlock<'a> {
         let set_clause_str = format!("SET {}", set_clause);
         let query_block = QueryBlock {
             query_part: set_clause_str,
-            secondary_part: &None,
-            statement_type: self.statement_type.clone()
+            secondary_part: None,
+            statement_type: self.statement_type.clone(),
         };
-        let latest_node = traverse_to_the_latest_node(self);
-        latest_node.secondary_part = &Some(Box::new(query_block));
+        let latest_node = traverse_to_the_latest_node(&mut self);
+        latest_node.secondary_part = Some(Box::new(query_block));
         self
     }
-    
-    fn where_clause(&mut self, where_clause: &String) -> &QueryBlock {
+
+    fn where_clause(mut self, where_clause: &String) -> Self {
         let where_clause_str = format!("WHERE {}", where_clause);
 
         let query_block = QueryBlock {
             query_part: where_clause_str,
-            secondary_part: &None,
-            statement_type: self.statement_type.clone()
+            secondary_part: None,
+            statement_type: self.statement_type.clone(),
         };
-        let latest_node = traverse_to_the_latest_node(self);
-        latest_node.secondary_part = &Some(Box::new(query_block));
+        let latest_node = traverse_to_the_latest_node(&mut self);
+        latest_node.secondary_part = Some(Box::new(query_block));
         self
     }
 }
 
-
-fn select<'a>(model: &Model) -> QueryBlock {
+fn select(model: &Model) -> QueryBlock {
     match &model.fields {
         Some(fields) => QueryBlock {
             query_part: format!("SELECT {} FROM {}", fields.join(", "), model.name),
             statement_type: StatementType::Select,
-            secondary_part: &None,
+            secondary_part: None,
         },
         None => QueryBlock {
             query_part: format!("SELECT * FROM {}", model.name),
             statement_type: StatementType::Select,
-            secondary_part: &None
-        }
+            secondary_part: None,
+        },
     }
 }
-
 
 fn update(model: &Model) -> QueryBlock {
     match &model.fields {
         Some(fields) => QueryBlock {
             query_part: format!("UPDATE {}", model.name),
             statement_type: StatementType::Update,
-            secondary_part: &None
+            secondary_part: None,
         },
-        None => panic!("Update query must have fields")
+        None => panic!("Update query must have fields"),
     }
 }
-
 
 fn delete(model: &Model) -> QueryBlock {
     QueryBlock {
         query_part: format!("DELETE FROM {}", model.name),
         statement_type: StatementType::Delete,
-        secondary_part: &None
+        secondary_part: None,
     }
 }
-
 
 fn insert(model: &Model) -> QueryBlock {
     let model = match &model.fields {
         Some(fields) => QueryBlock {
             query_part: format!("INSERT INTO {} ({})", model.name, fields.join(", ")),
             statement_type: StatementType::Insert,
-            secondary_part: &None
+            secondary_part: None,
         },
         None => QueryBlock {
             query_part: format!("INSERT INTO {}", model.name),
             statement_type: StatementType::Insert,
-            secondary_part: &None
-        }
+            secondary_part: None,
+        },
     };
     model
 }
 
-fn traverse_to_the_latest_node<'a>(statement: &mut QueryBlock) -> &'a mut QueryBlock<'a> {
+fn traverse_to_the_latest_node(statement: &mut QueryBlock) -> &mut QueryBlock {
     let mut ret = statement;
     while ret.secondary_part.is_some() {
         ret = ret.secondary_part.as_mut().unwrap();
@@ -134,38 +127,45 @@ fn traverse_to_the_latest_node<'a>(statement: &mut QueryBlock) -> &'a mut QueryB
     ret
 }
 
-
 fn compile_statement(statement: &QueryBlock) -> String {
     fn helper(statement: &QueryBlock, acc: String) -> String {
         match &statement.secondary_part {
             Some(next_node) => helper(next_node, acc + &statement.query_part),
-            None => acc + " " + &statement.query_part
+            None => acc + " " + &statement.query_part,
         }
     }
     helper(statement, "".to_string())
 }
-
 
 mod tests {
     use super::*;
 
     #[test]
     fn test_select() {
-        let model = Model { name: "users".to_string(), fields: None };
+        let model = Model {
+            name: "users".to_string(),
+            fields: None,
+        };
         let query = select(&model);
         assert_eq!(query.query_part, "SELECT * FROM users");
     }
 
     #[test]
     fn test_select_with_fields() {
-        let model = Model { name: "users".to_string(), fields: Some(vec!["id".to_string(), "name".to_string()]) };
+        let model = Model {
+            name: "users".to_string(),
+            fields: Some(vec!["id".to_string(), "name".to_string()]),
+        };
         let query = select(&model);
         assert_eq!(query.query_part, "SELECT id, name FROM users");
     }
 
     #[test]
     fn test_insert_values() {
-        let model = Model { name: "users".to_string(), fields: None };
+        let model = Model {
+            name: "users".to_string(),
+            fields: None,
+        };
         let mut query = insert(&model).values(&vec!["1".to_string(), "John".to_string()]);
         let compiled_query = compile_statement(&query);
         assert_eq!(compiled_query, "INSERT INTO users VALUES (1, John)");
@@ -173,7 +173,10 @@ mod tests {
 
     #[test]
     fn test_select_where_clause() {
-        let model = Model { name: "users".to_string(), fields: None };
+        let model = Model {
+            name: "users".to_string(),
+            fields: None,
+        };
         let mut binding = select(&model);
         let query = binding.where_clause(&"id = 1".to_string());
         let compiled_query = compile_statement(&query);
@@ -182,7 +185,10 @@ mod tests {
 
     #[test]
     fn test_update_values() {
-        let model = Model { name: "users".to_string(), fields: Some(vec!["name".to_string()]) };
+        let model = Model {
+            name: "users".to_string(),
+            fields: Some(vec!["name".to_string()]),
+        };
         let mut binding = update(&model);
         let mut arguments = HashMap::new();
         arguments.insert("name".to_string(), "John".to_string());
@@ -196,11 +202,12 @@ mod tests {
         let mut arguments = HashMap::new();
         arguments.insert("id".to_string(), "2".to_string());
         let mut where_string = "id = 1".to_string();
-        let model = Model { name: "users".to_string(), fields: None };
-        let mut query = update(&model).where_clause(&where_string).set(&arguments);;
+        let model = Model {
+            name: "users".to_string(),
+            fields: None,
+        };
+        let mut query = update(&model).where_clause(&where_string).set(&arguments);
         let compiled_query = compile_statement(&query);
         assert_eq!(compiled_query, "UPDATE users SET id = 2 WHERE id = 1");
     }
 }
-
-    
